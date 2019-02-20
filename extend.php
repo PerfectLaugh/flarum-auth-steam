f9043315823af1ed81d3622fcd1a1ab2e9ba2a6f
PerfectLaugh <denniswu81229@gmail.com>
2019-02-20 18:18:13 +0800

    Locale fix

--- extend.php
+++ extend.php
@@ -11,6 +11,8 @@ return [
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
 
+    new Extend\Locales(__DIR__ . '/locale'),
+
     (new Extend\Routes('forum'))
         ->get('/auth/steam', 'auth.steam', SteamAuthController::class),
 ];
